272606c39c260956273d73153c5a4e60d84b8e7a
robertkutwa <robertkutwa0@gmail.com>
2025-01-26 10:14:16 +0300

    add akan names

--- java.js/java.js
+++ java.js/java.js
@@ -23,7 +23,6 @@ document.getElementById('akan-form').addEventListener('submit', function (e) {
       return;
     }
   
-    // Akan names
     const akanNames = {
       male: ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"],
       female: ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
@@ -33,8 +32,7 @@ document.getElementById('akan-form').addEventListener('submit', function (e) {
   
     const akanName = akanNames[gender][dayOfWeek];
     const dayName = days[dayOfWeek];
-  
-    // Display result
+
     const result = document.getElementById('result');
     result.textContent = `You were born on a ${dayName}, and your Akan name is ${akanName}.`;
   });
